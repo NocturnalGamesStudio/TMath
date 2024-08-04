aedc6730086fe333d3246ae913206aa041656bf7
Nocturnal <nocturnalgamesstudio@proton.me>
2024-08-04 18:51:22 +0100

    Added readme

--- build_lib.sh
+++ build_lib.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
+if type gcc.exe &> /dev/null; then
+    alias gcc=gcc.exe
+    alias ar=ar.exe
+fi
+
+
+
 src_files="$(find -iwholename "./src/*.c" -print0 | xargs -0)"
 inc="-I./inc"
 objs_out="./out/Tmath"
 
-gcc.exe $inc -c $src_files -std=c17
+gcc $inc -c $src_files -std=c17
 
 objs="$(find -iname "*.o" -print0 | xargs -0)"
 
-ar.exe rcs ./out/lib $objs
+ar rcs ./out/lib $objs
 
 rm *.o
--- build_tests.sh
+++ build_tests.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+if type gcc.exe &> /dev/null; then
+    alias gcc=gcc.exe
+fi
+
 static_libs="./out/lib"
 src_files="$(find -iwholename "./tests/*.c" -print0 | xargs -0) $static_libs"
 inc="-I./inc"
 out="-o ./out/tests.exe"
 
-gcc.exe $inc $src_files $out
+gcc $inc $src_files $out
